1897c86e2ba85da442c6490d2170d1d0a1ce12d5
Amartey Pearson <apearson@us.ibm.com>
2014-10-20 18:52:49 -0500

    Initial copy of legacy VMC driver - this will not compile yet.
    Change-Id: I5f74068641f82cee712efca17212588960f5aa0b

---
+++ test/test2.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int vmc_fd;
+
+// DRENG need to add more EBUSY checking
+
+int vmc_open() {
+        vmc_fd = open("/dev/ibmvmc", O_RDWR);
+        printf("vmc_open: vmc_fd = 0x%x\n", vmc_fd);
+
+        return vmc_fd;
+}
+
+int vmc_send_hmc_id() {
+        char hmc_id[32] = "i am an hmc id";
+        int rc;
+
+        rc = ioctl(vmc_fd, 1, hmc_id);
+
+        printf("vmc_send_hmc_id: rc = 0x%x\n", rc);
+}
+
+int vmc_send_msg() {
+        char msg[32] = "this is an hmc msg";
+        int rc;
+        int c = 0;
+
+        do {
+                rc = write(vmc_fd, msg, 32);
+                if(c == 0)
+                        printf("vmc_send_msg: rc = 0x%x, errno = %d\n", rc, errno);
+                c++;
+                if(c == 100) c = 0;
+        } while ((rc == -1) && (errno == EBUSY));
+
+        printf("vmc_send_msg: complete.\n");
+}
+
+main () {
+        printf("Starting VMC test\n");
+
+        if(vmc_open() >= 0) {
+                vmc_send_hmc_id();
+                vmc_send_msg();
+        } else {
+                printf("vmc open failed: %d\n", errno);
+        }
+}
+
+
