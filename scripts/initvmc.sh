e048f50adc816d32d0351e1ad26d5ac2029242e3
Steven Royer <seroyer@us.ibm.com>
2014-12-01 16:48:43 -0600

    Script to load module and create dev node
    Change-Id: I51192fbd03991c512208715ad28486cd3fdb65b8

---
+++ scripts/initvmc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DEVICE=ibmvmc
+
+# Load module
+modprobe $DEVICE
+if [ $? -ne 0 ]; then
+    echo "Error loading $DEVICE module\n"
+    exit 1
+fi
+
+# Create device
+if [ ! -c /dev/$DEVICE ]; then
+    MAJOR=`sed -n 's/\([0-9]\+\) ibmvmc/\1/p' /proc/devices`
+    if [ -z "$MAJOR" ]; then
+	echo "Error getting $DEVICE major number\n"
+	rmmod $DEVICE
+	exit 1
+    fi
+
+    # TODO what should be permissions and ownership be?
+    mknod -m 0660 /dev/$DEVICE c $MAJOR 0
+    if [ $? -ne 0 ]; then
+	echo "Error creating $DEVICE\n"
+	rmmod $DEVICE
+	exit 1
+    fi
+fi
+
+
